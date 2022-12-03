f8eb4f876140ca57ef188a47c9c8fcbf2f0000f0
Kristoffer Moberg Christensen <kristoffermoberg.christensen@skatteetaten.no>
2022-12-04 00:00:09 +0100

    First two days of AOC2022

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,62 @@
+data class Elf(
+    val elfNumber: Int,
+    val calories: List<Int> = emptyList()
+) {
+    constructor(elfIndex: Int, caloriesString: String) : this(
+        elfNumber = elfIndex + 1,
+        calories = caloriesString.split(",")
+            .filter { it != "" }
+            .map { it.toInt() })
+
+    val totalCalories = calories.sum()
+}
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun parseElves(input: List<String>): List<Elf> {
+        return input.joinToString(",") {
+            if (it == "") "|" else it
+        }.split("|")
+            .mapIndexed { elfNumber, elfCalories ->
+                Elf(elfNumber, elfCalories)
+            }
+    }
+
+    fun part1(input: List<String>): Elf {
+        val allElves = parseElves(input)
+
+        return allElves.maxBy { it.totalCalories }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val allElves = parseElves(input)
+
+        return allElves.sortedByDescending { it.totalCalories }.take(3).sumOf { it.totalCalories }
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testOutput = part1(testInput)
+    println("----SIMPLE TESTING----")
+    println("Elf with top total calories is $testOutput")
+    check(testOutput.elfNumber == 4)
+
+    val testOutput2 = part2(testInput)
+    println("Total amount of calories for 3 top elves is $testOutput2")
+    check(testOutput2 == 45000)
+
+    println()
 
     val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    val part1Output = part1(input)
+
+    println("----PART1----")
+
+    check(part1Output.elfNumber == 61)
+    println("Elf with top total calories is $part1Output")
+    val part2Output = part2(input)
+
+    println()
+    println("----PART2----")
+
+    println("Total amount of calories for 3 top elves is $part2Output")
 }
---
+++ src/Day02.kt
@@ -0,0 +1,139 @@
+import java.lang.IllegalArgumentException
+import java.lang.IllegalStateException
+import PlayOutcome.*
+
+enum class PlayOutcome(val codeLetter: Char, val scoreValue: Int) {
+    Loose('X', 0),
+    Draw('Y', 3),
+    Win('Z', 6);
+
+    companion object {
+        fun from(codeLetter: Char): PlayOutcome {
+            return values().first { it.codeLetter == codeLetter }
+        }
+    }
+}
+
+enum class Shape(val scoreValue: Int, val codeLetter: Char) {
+    Rock(scoreValue = 1, codeLetter = 'A') {
+        override val beatsShape: Shape
+            get() = Scissor
+    },
+    Paper(scoreValue = 2, codeLetter = 'B') {
+        override val beatsShape: Shape
+            get() = Rock
+    },
+    Scissor(scoreValue = 3, codeLetter = 'C') {
+        override val beatsShape: Shape
+            get() = Paper
+    };
+
+    abstract val beatsShape: Shape
+
+    fun playAgainst(other: Shape): PlayOutcome {
+        return when {
+            this.beatsShape == other -> Win
+            this == other -> Draw
+            other.beatsShape == this -> Loose
+            else -> throw IllegalStateException("Outcome not supported")
+        }
+    }
+
+    companion object {
+        fun from(codeLetter: Char): Shape {
+            return Shape.values().first { it.codeLetter == codeLetter }
+        }
+
+        fun fromMyCode(codeLetter: Char): Shape {
+            return when (codeLetter) {
+                'X' -> Rock
+                'Y' -> Paper
+                'Z' -> Scissor
+                else -> throw IllegalArgumentException("Invalid code letter $codeLetter")
+            }
+        }
+
+        fun fromMyWinningStrategy(strategy: PlayOutcome, foeShape: Shape): Shape {
+            return when (strategy) {
+                Win -> Shape.values().first { it.beatsShape == foeShape }
+                Loose -> foeShape.beatsShape
+                Draw -> foeShape
+            }
+        }
+    }
+}
+
+enum class RPS(
+    val foeCodeLetter: Char,
+    val myCodeLetter: Char,
+    val scoreValue: Int
+) {
+    Rock('A', 'X', 1),
+    Paper('B', 'Y', 2),
+    Scissor('C', 'Z', 3);
+
+    companion object {
+        fun from(codeLetter: Char): RPS {
+            return RPS.values().first { it.foeCodeLetter == codeLetter || it.myCodeLetter == codeLetter }
+        }
+
+        fun fromPlayStrategy(strategy: Char, foeChooses: RPS) {
+            when (strategy) {
+                'X' -> TODO()
+                'Y' -> TODO()
+                'Z' -> TODO()
+            }
+        }
+    }
+}
+
+fun main() {
+    fun part1(input: List<String>): Int {
+        return input.sumOf {
+            val foe = Shape.from(it.first())
+            val me = Shape.fromMyCode(it.last())
+
+            val matchScore = me.playAgainst(foe).scoreValue
+
+            return@sumOf me.scoreValue + matchScore
+        }
+    }
+
+    fun part2(input: List<String>): Int {
+        return input.sumOf {
+            val foe = Shape.from(it.first())
+            val strategy = PlayOutcome.from(it.last())
+            val me = Shape.fromMyWinningStrategy(strategy, foe)
+
+            val matchScore = me.playAgainst(foe).scoreValue
+
+            return@sumOf me.scoreValue + matchScore
+        }
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day02_test")
+    val testOutput = part1(testInput)
+    println("----SIMPLE TESTING----")
+    println(testOutput)
+    check(testOutput == 15)
+
+
+    println()
+
+    val input = readInput("Day02")
+
+    println("----PART1----")
+    val part1Output = part1(input)
+
+    println("Total score for me is $part1Output")
+
+    val testOutput2 = part2(testInput)
+    check(testOutput2 == 12)
+
+    println()
+    println("----PART2----")
+
+    val part2Output = part2(input)
+    println("Total amount of score for Rock Paper Scissor is $part2Output")
+}
